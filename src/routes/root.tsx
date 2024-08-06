08686429f2e0f40ff937f4b2f8945743b692b592
shiwilsahid <sieengelmein@gmail.com>
2024-08-06 14:10:17 +0700

    feat: add route home and testing new branch

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { Outlet } from "react-router-dom";
 
 export function RootRoute() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold">
-        Welcome to <span className="text-red-500"> masakmudah.com </span>
-      </h1>
-      <Button asChild className="w-40 h-16 bg-red-500 mt-4">
-        <a href="/" className="">
-          <img
-            src="/images/logo-masakmudah.png"
-            alt="logo-masakmudah.png"
-            className="h-full object-contain"
-          />
-        </a>
-      </Button>
+    <div className="flex flex-col h-screen">
+      <div>
+        <header></header>
+      </div>
+
+      <div>
+        <Outlet />
+      </div>
+
+      <div>
+        <footer></footer>
+      </div>
     </div>
   );
 }
