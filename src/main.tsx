5016c18b616ec4a1e0be40b11da16b6681259c9b
shiwilsahid <sieengelmein@gmail.com>
2024-08-06 14:14:38 +0700

    feat: add LoginRoute and second test branch

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootRoute } from "./routes/root";
 import { HomeRoute } from "./routes/home";
+import { LoginRoute } from "./routes/login";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <HomeRoute />,
+      },
+      {
+        path: "/login",
+        element: <LoginRoute />,
       },
     ],
   },
