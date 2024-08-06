222015d5cb60bbd34a0a078f3ce58bca04e9ca43
shiwilsahid <sieengelmein@gmail.com>
2024-08-06 14:10:17 +0700

    feat: add route home and testing new branch

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootRoute } from "./routes/root";
+import { HomeRoute } from "./routes/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootRoute />,
+    children: [
+      {
+        path: "/",
+        element: <HomeRoute />,
+      },
+    ],
   },
 ]);
 
