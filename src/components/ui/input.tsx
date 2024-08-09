0dba25e39ec27e8a99439b090985de095bd25fde
shiwilsahid <sieengelmein@gmail.com>
2024-08-09 16:04:39 +0700

    feat: fix typing text to font

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-80 capitalize rounded-xl border-2 border-gray-200 bg-transparent px-3 py-2 text-base text-semibold text-gray-400 ring-none file:border-0 file:bg-transparent file:text-base file:font-semibold placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-80 capitalize rounded-xl border-2 border-gray-200 bg-transparent px-3 py-2 text-base font-semibold text-gray-400 ring-none file:border-0 file:bg-transparent file:text-base file:font-semibold placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
