4adb011fd2826428458ff625da5eeb5eb76184c3
shiwilsahid <sieengelmein@gmail.com>
2024-08-21 03:07:12 +0700

    feat: refine styling on header, footer, and router

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -6,6 +6,14 @@ import { useForm } from "react-hook-form";
 import { searchSchema } from "@/libs/zod-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import Container from "../ui/container";
+import { AlignJustify } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export function Header() {
   const navigate = useNavigate();
@@ -37,27 +45,54 @@ export function Header() {
   };
 
   return (
-    <header className="h-20 w-full flex items-center justify-center text-[#202633]">
-      <div className=" max-w-screen-xl w-full flex items-center justify-between px-8">
-        <div id="left-side" className="flex items-center gap-x-20">
-          <Link to="/" className="flex items-center gap-x-2">
-            <h1 className="text-xl font-clashDisplaySemibold">Masakmudah</h1>
+    <header className="bg-[#E6FDB0] shadow-sm w-full">
+      <Container className="flex items-center justify-between md:gap-20 p-4 md:p-6 space-x-2 w-full max-w-full md:w-fit">
+        <div id="left-side" className="flex items-center gap-x-4 md:gap-x-20">
+          <Link to="/" className="hidden md:flex items-center gap-x-2">
+            <h1 className="text-lg md:text-xl font-clashDisplaySemibold">
+              Masakmudah
+            </h1>
           </Link>
-          <nav>
-            <ul className="flex items-center gap-x-10">
-              {navigationMenu.map((menu) => (
-                <li key={menu.href}>
-                  <Link to={menu.href} className="font-clashDisplayMedium">
-                    {menu.label}
+
+          <div className="md:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <AlignJustify className="cursor-pointer" /> {/* Icon */}
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mt-4 ml-2 w-56 bg-[#1C2625]">
+                {navigationMenu.map((menu) => (
+                  <Link key={menu.href} to={menu.href}>
+                    <DropdownMenuItem className="text-base text-white font-clashDisplayMedium">
+                      {menu.label}
+                    </DropdownMenuItem>
                   </Link>
-                </li>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+
+          <Link to="/" className="md:hidden">
+            <h1 className="text-lg font-clashDisplaySemibold">Masakmudah</h1>
+          </Link>
+
+          <nav className="hidden md:flex">
+            <ul className="flex items-center gap-x-6 md:gap-x-10">
+              {navigationMenu.map((menu) => (
+                <Link key={menu.href} to={menu.href}>
+                  <li className="text-sm md:text-base font-clashDisplayMedium">
+                    {menu.label}
+                  </li>
+                </Link>
               ))}
             </ul>
           </nav>
         </div>
-        <div id="right-side" className="flex items-center gap-x-4">
+        <div id="right-side" className="flex items-center gap-x-4 md:gap-x-16">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full md:w-auto"
+            >
               <FormField
                 control={form.control}
                 name="search"
@@ -65,7 +100,7 @@ export function Header() {
                   <FormItem>
                     <Input
                       placeholder="Cari resep"
-                      className="pl-6 bg-white rounded-3xl focus-visible:ring-0 focus-visible:ring-offset-0 border-2 w-96 placeholder:text-slate-400"
+                      className="pl-6 bg-white rounded-3xl focus-visible:ring-0 focus-visible:ring-offset-0 border-2 w-full md:w-96 placeholder:text-slate-400"
                       {...field}
                     />
                   </FormItem>
@@ -75,12 +110,12 @@ export function Header() {
           </Form>
           <Link
             to="/login"
-            className="px-8 py-2 bg-[#FF5D47] rounded-3xl font-raleway text-white"
+            className="px-4 md:px-8 py-2 md:py-2 bg-[#FF5D47] rounded-3xl font-raleway text-white text-sm md:text-base"
           >
             Login
           </Link>
         </div>
-      </div>
+      </Container>
     </header>
   );
 }
