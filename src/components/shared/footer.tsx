573af61b38cbf63fc2a2e2bd4cf852299de0051e
M Nur Sahid <80140140+shiwilsahid@users.noreply.github.com>
2024-08-21 03:14:13 +0700

    feat: refine styling on header, footer, and router (#25)
    It will be refined later.

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -3,30 +3,36 @@ import Container from "../ui/container";
 
 export const Footer = () => {
   return (
-    <footer className=" bg-[#1C2625] text-white font-raleway">
-      <Container className="flex justify-between">
+    <footer className=" bg-[#1C2625] text-white font-clashDisplayRegular w-full">
+      <Container className="flex flex-col md:flex-row justify-between space-y-16 md:space-y-0">
         <div className="space-y-2 max-w-xl">
-          <h1 className="text-2xl font-clashDisplaySemibold">Masakmudah</h1>
+          <h1 className="text-xl md:text-2xl font-clashDisplaySemibold">
+            Masakmudah
+          </h1>
           <p>
             Temukan berbagai resep dan buat masakan dengan mudah hanya di
             Masakmudah
           </p>
         </div>
-        <div className="flex gap-x-16">
+        <div className="flex flex-col md:flex-row gap-x-16 md:gap-x-16 space-y-6 md:space-y-0">
           <div className="space-y-2">
-            <h3 className="font-bold mb-4 font-clashDisplayMedium">Sitemap</h3>
+            <h3 className="font-bold mb-2 md:mb-4 font-clashDisplayMedium">
+              Sitemap
+            </h3>
             <Link to="/">Home</Link>
             <p>Semua resep</p>
             <p>Tentang</p>
           </div>
           <div className="space-y-2">
-            <h3 className="font-bold mb-4 font-clashDisplayMedium">Bantuan</h3>
+            <h3 className="font-bold mb-2 md:mb-4 font-clashDisplayMedium">
+              Bantuan
+            </h3>
             <p>Kontak</p>
             <p>Lapor resep berbahaya</p>
             <p>Tentang</p>
           </div>
           <div className="space-y-2">
-            <h3 className="font-bold mb-4 font-clashDisplayMedium">
+            <h3 className="font-bold mb-2 md:mb-4 font-clashDisplayMedium">
               Sosial media
             </h3>
             <p>Instagram</p>
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/shared/footer";
 import { Header } from "@/components/shared/header";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 
 export function RootRoute() {
@@ -7,13 +8,20 @@ export function RootRoute() {
   const notRoutes = ["/login", "/register"];
 
   return (
-    <>
-      {!notRoutes.includes(location.pathname) && <Header />}
+    <div className="flex-grow bg-[#FDFFF7]">
+      {!notRoutes.includes(location.pathname) && (
+        <div>
+          <Header />
+          <Separator className="h-[1px] bg-[#1C2625]" />
+        </div>
+      )}
+
       <main className="flex flex-col min-h-screen bg-[#FDFFF7]">
         <ScrollRestoration />
         <Outlet />
       </main>
+
       {!notRoutes.includes(location.pathname) && <Footer />}
-    </>
+    </div>
   );
 }
