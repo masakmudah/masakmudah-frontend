af1a8091c627a06b0b2a86a86bfa75a3a68f372f
shiwilsahid <sieengelmein@gmail.com>
2024-08-26 21:29:53 +0700

    feat: refine UI and remove unnecessary button

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 
 export function DashboardRoute() {
   return (
-    <main className="flex justify-center">
-      <div className="mt-20 ">
-        <section className="flex justify-center max-w-5xl">
+    <div className="">
+      <div className="flex flex-col w-full max-w-5xl mx-auto p-4 sm:p-6 md:p-8 lg:p-10  rounded-lg gap-5 text-black">
+        <section className="flex justify-start gap-10">
           <Link to="/dashboard">
             <Avatar className="w-32 h-32">
               <img
@@ -17,30 +17,30 @@ export function DashboardRoute() {
             </Avatar>
           </Link>
 
-          <div className="ml-4 mt-2">
-            <div>
-              <Link to="/dashboard">
-                <p className="hover:text-blue-800">@username_user</p>
-              </Link>
-              <h1>fullname_user</h1>
-            </div>
-            <div className="mt-5 flex">
+          <div className="justify-center items-start flex flex-col">
+            <Link to="/dashboard">
+              <p className="hover:text-blue-800">@username_user</p>
+            </Link>
+            <h1>fullname_user</h1>
+          </div>
+          {/* <div className="">
+            <div className="mt- flex">
               <p>This is my bio</p>
             </div>
-          </div>
+          </div> */}
 
-          <div className="ml-4 mt-2">
+          {/* <div className="ml-4 mt-2">
             <Button asChild className="mt-4">
               <a href="/editprofile">Edit Profile</a>
             </Button>
-          </div>
+          </div> */}
         </section>
 
-        <div className="mt-4">
+        {/* <div className="mt-4">
           <Button>Aktifitas</Button>
           <Button className="ml-4">Resepku</Button>
-        </div>
-        <Separator className="my-4 h-1 bg-gray-400" />
+        </div> */}
+        <Separator className="h-1 bg-gray-400" />
 
         <section className="text-center">
           <p>Belum punya resep</p>
@@ -49,6 +49,6 @@ export function DashboardRoute() {
           </Button>
         </section>
       </div>
-    </main>
+    </div>
   );
 }
