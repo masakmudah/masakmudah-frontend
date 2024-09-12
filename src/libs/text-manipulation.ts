e9a22b1b2cb3b25349391221da4bf59dfe190ac8
shiwilsahid <sieengelmein@gmail.com>
2024-09-13 01:06:36 +0700

    fix: error URL non-Uploadcare

--- src/libs/text-manipulation.ts
+++ src/libs/text-manipulation.ts
@@ -4,9 +4,13 @@ export const resizeUploadcareImage = (
   height: number = 400
 ): string => {
   if (url.startsWith("https://ucarecdn.com/")) {
-    return `${url}-/preview/${width}x${height}/`;
+    const getUUID = url.split("/")[3];
+    const URL = `https://ucarecdn.com/${getUUID}`;
+    const result = `${URL}/-/preview/${width}x${height}/`;
+    // return `${url}-/preview/${width}x${height}/`;
+    return result;
   }
   // Tambahkan penanganan untuk URL non-Uploadcare
-  console.warn("URL bukan dari Uploadcare:", url);
+  // console.warn("URL bukan dari Uploadcare:", url);
   return url;
 };
